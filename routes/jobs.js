946a9b305e656b1129d725bdc5672aedd51a2674
Dorian Thompson <dorianthompson2@Dorians-MBP.fios-router.home>
2023-07-22 23:36:05 -0400

    initial server setup

---
+++ routes/jobs.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+} = require('../controllers/jobs');
+
+
+router.route('/').post(createJob).get(getAllJobs)
+router.route('/:id').post(createJob).get(getJob).patch(updateJob).delete(deleteJob);
+
+module.exports = router;
+
